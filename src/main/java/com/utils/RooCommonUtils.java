1c9ef967e05df31f8b3c4e1182153df6c488d45a
yangsheng <1821332070@qq.com>
2018-09-12 17:02:55 +0800

    公共方法添加-浮点转化

--- src/main/java/com/utils/RooCommonUtils.java
+++ src/main/java/com/utils/RooCommonUtils.java
@@ -7,6 +7,25 @@ import com.alibaba.fastjson.JSONObject;
 
 public class RooCommonUtils {
 	
+	/*数据转化成0.00格式*/
+	public static String decimalConvert(String sum) {
+		if(sum.contains(".")) {
+			int index = sum.indexOf(".");
+			int length = sum.length();
+			System.out.println("index:"+index);
+			System.out.println("length:"+length);
+			
+			if(length - index == 2) {
+				sum = sum + "0";
+			}else if (length - index == 1) {
+				sum = sum + "00";
+			}
+		}else {
+			sum += ".00";
+		}
+		return sum;
+	}
+	
 	/*按key，获取json值*/
 	public static String getJsonValue(JSONObject jsonObject , String key) {
 		if(jsonObject == null || jsonObject.get(key) == null || jsonObject.get(key) == "") {
