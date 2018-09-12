e33679e75f90bf9b2d5dc57564001de0e0d253d6
yangsheng <1821332070@qq.com>
2018-09-12 16:53:41 +0800

    定义系统异常

--- src/main/java/com/result/CodeMsg.java
+++ src/main/java/com/result/CodeMsg.java
@@ -4,6 +4,17 @@ public class CodeMsg {
 	private int code;
 	private String msg;
 	
+	//定义系统错误
+	public static CodeMsg INTF_SV_ERROR = new CodeMsg(101, "服务接口异常");
+	public static CodeMsg INTF_PARAM_IN_ERROR = new CodeMsg(102, "服务接口入参异常");
+	
+	
+	
+	
+	
+	
+	
+	
 	//通用返回
 	public static CodeMsg SUCCESS = new CodeMsg(0, "success");
 	public static CodeMsg SERVER_ERROR = new CodeMsg(-999, "服务端异常");
@@ -90,7 +101,6 @@ public class CodeMsg {
 		this.code = code;
 		this.msg = msg;
 	}
-	
 	
 	public int getCode() {
 		return code;
